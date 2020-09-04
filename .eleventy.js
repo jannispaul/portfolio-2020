8630436c7e2b42457733091c874a0789b3808db3
Jannis <jannis@pixel-pink.de>
2020-09-04 17:39:42 +0200

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+const CleanCSS = require("clean-css");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+  eleventyConfig.addLayoutAlias("default", "layouts/default.njk");
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  return {
+    templateFormats: ["md", "njk"],
+
+    dir: {
+      input: "src/",
+      includes: "_includes",
+      output: "_site",
+    },
+
+    passthroughFileCopy: true,
+  };
+};
